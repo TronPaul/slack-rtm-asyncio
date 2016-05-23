b8c097c43cbbae878355e59bfb2a9064ea56a46c
Mark McGuire <mmcguire@backstopsolutions.com>
2016-05-22 22:50:08 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='slack-rtm-asyncio',
+    version='0.0.1',
+    packages=['slack_rtm'],
+    url='',
+    license='',
+    author='Mark McGuire',
+    author_email='',
+    description='',
+    install_requires=['venusian>=1.0', 'websockets>=3.1']
+)
